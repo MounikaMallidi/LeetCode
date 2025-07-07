3f0ec5f88e9079c34e28e4b375b7129ed22f94ea
M.Mounika <166210028+MounikaMallidi@users.noreply.github.com>
2025-07-07 22:41:59 +0530

    Time: 0 ms (100%), Space: 8.5 MB (68.21%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int temp=x,rem;
+        long rev=0;
+        while(x>0){
+            rem=x%10;
+            rev=rev*10+rem;
+            x/=10;
+        }
+        if(temp==rev){
+            return true;
+        }
+        return false;
+    }
+};
