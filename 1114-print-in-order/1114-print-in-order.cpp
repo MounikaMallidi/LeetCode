e9b4b0f6e3fd2f52585cae0b6b2390def4c99113
M.Mounika <166210028+MounikaMallidi@users.noreply.github.com>
2025-07-19 22:13:43 +0530

    Time: 4 ms (69.45%), Space: 9.6 MB (6%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+class Foo {
+private:
+    mutex mtx;
+    condition_variable cv;
+    int state;
+
+public:
+    Foo() {
+        state = 0;
+    }
+
+    void first(function<void()> printFirst) {
+        unique_lock<mutex> lock(mtx);
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        state = 1;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [this]() { return state >= 1; });
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        state = 2;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [this]() { return state >= 2; });
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};
