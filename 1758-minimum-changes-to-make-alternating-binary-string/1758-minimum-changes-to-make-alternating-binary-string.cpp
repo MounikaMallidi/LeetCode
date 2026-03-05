3605346672515cea84a059a7f8f6c8471b0e7bee
Mallidi Mounika <23a91a0436@aec.edu.in>
2026-03-05 21:23:39 +0530

    Time: 3 ms (17.69%), Space: 10.4 MB (6.11%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int cnt=0;
+        string s1,s2;
+        for(int i=0;i<s.size();i++){
+            if(i%2==0){
+                s1+='0';
+                s2+='1';
+            }
+            else{
+                s1+='1';
+                s2+='0';
+            }
+        }
+        int cnt1=0,cnt2=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=s1[i]){
+                cnt1++;
+            }
+            if(s[i]!=s2[i]){
+                cnt2++;
+            }
+        }
+        return min(cnt1,cnt2);
+    }
+};
