92939dba8ccdc5724a2bb44988e53a530b249f9c
Mallidi Mounika <23a91a0436@aec.edu.in>
2026-03-07 21:39:47 +0530

    Time: 0 ms (100%), Space: 27.3 MB (27.47%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        vector<int> prefix(n);
+        vector<int> suffix(n);
+        prefix[0]=height[0];
+        suffix[n-1]=height[n-1];
+        for(int i=1;i<n;i++){
+            prefix[i]=max(prefix[i-1],height[i]);
+        }
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=max(suffix[i+1],height[i]);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int leftmax=prefix[i],rightmax=suffix[i];
+            if(height[i]<leftmax && height[i]<rightmax){
+                ans+=min(leftmax,rightmax)-height[i];
+            }
+        }
+        return ans;
+;    }
+};
