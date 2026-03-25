081bbf22037bd4677f6efeb026d0babf66f1482f
Mallidi Mounika <23a91a0436@aec.edu.in>
2026-03-25 16:06:01 +0530

    Time: 45 ms (52.04%), Space: 93.9 MB (66.52%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),[](vector<int>& a,vector<int>& b){
+            return a[1]<b[1];
+        });
+        int n=intervals.size();
+        int cnt=0;
+        int lastEnd=intervals[0][1];
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<lastEnd){
+                cnt++;
+            }
+            else{
+                lastEnd=intervals[i][1];
+            }
+        }
+        return cnt;
+    }
+};
