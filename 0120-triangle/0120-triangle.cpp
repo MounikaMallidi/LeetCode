e64f4111fddef8c2c778ea9a416224c40c30574b
Mallidi Mounika <23a91a0436@aec.edu.in>
2026-05-22 12:06:43 +0530

    Time: 0 ms (100%), Space: 13.5 MB (7.98%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,11 +2,17 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
+        vector<vector<int>> DP(n,vector<int>(n));
+        for(int i=0;i<n;i++){
+            DP[n-1][i]=triangle[n-1][i];
+        }
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<=i;j++){
-                triangle[i][j]+=min(triangle[i+1][j],triangle[i+1][j+1]);
+                int x=DP[i+1][j]+triangle[i][j];
+                int y=DP[i+1][j+1]+triangle[i][j];
+                DP[i][j]=min(x,y);
             }
         }
-        return triangle[0][0];
+        return DP[0][0];
     }
 };
