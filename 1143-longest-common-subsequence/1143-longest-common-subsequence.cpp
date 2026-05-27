a26c8a8c53bb7c128f16d0bf047aad4da4312e90
Mallidi Mounika <23a91a0436@aec.edu.in>
2026-05-27 13:39:54 +0530

    Time: 24 ms (70.6%), Space: 27.4 MB (67.23%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,7 +3,7 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<vector<unsigned int>> dp(n+1,vector<unsigned int>(m+1,0));
         for(int j=0;j<=m;j++) dp[0][j]=0;
         for(int i=0;i<=n;i++) dp[i][0]=0;
         for(int i=1;i<=n;i++){
