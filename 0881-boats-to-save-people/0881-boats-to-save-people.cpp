90b8e89431adbc6e906ebcf74ab514ce08b889b2
Mallidi Mounika <23a91a0436@aec.edu.in>
2026-08-06 14:08:07 +0530

    Time: 14 ms (77.57%), Space: 45.7 MB (71.86%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int left=0,right=people.size()-1;
+        int boat=0;
+        while(left<=right){
+            while(left<right && people[left]+people[right]>limit){
+                boat++;
+                right--;
+            }
+            boat++;
+            left++;
+            right--;
+        }
+        return boat;
+
+    }
+};
