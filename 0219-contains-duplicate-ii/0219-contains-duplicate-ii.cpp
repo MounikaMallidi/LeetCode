02888a3b00bc2eed77b847c1e02a1b845934e6e5
Mallidi Mounika <23a91a0436@aec.edu.in>
2026-08-08 10:16:40 +0530

    Time: 99 ms (37.82%), Space: 124.8 MB (21.47%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> mpp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(mpp.find(nums[i])!=mpp.end() && abs(mpp[nums[i]]-i)<=k){
+                return true;
+            }
+            mpp[nums[i]]=i;
+        }
+        return false;
+    }
+};
