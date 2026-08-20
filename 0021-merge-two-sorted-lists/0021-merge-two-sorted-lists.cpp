0e3ab9b8d979bd94a5dc9322a055e2f19d83c3c4
Mallidi Mounika <23a91a0436@aec.edu.in>
2026-08-20 06:21:52 +0530

    Time: 0 ms (100%), Space: 19.6 MB (27.83%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -14,13 +14,25 @@ public:
         if(list1 == NULL) return list2;
         if(list2 == NULL) return list1;
 
-        if(list1->val <= list2->val) {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
+        ListNode* dummy=new ListNode(0);
+        ListNode* temp=dummy;
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val<=list2->val){
+                temp->next=list1;
+                list1=list1->next;
+            }
+            else{
+                temp->next=list2;
+                list2=list2->next;
+            }
+            temp=temp->next;
         }
-        else {
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
+        if(list1!=NULL){
+            temp->next=list1;
         }
+        else{
+            temp->next=list2;
+        }
+        return dummy->next;
     }
 };
